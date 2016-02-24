10c549ffc0a61700c4ae6eb433466f7a7a54a1bc
kyorohiro <kyorohiro@gmail.com>
2016-02-25 06:36:02 +0900

    ..

--- example_todo/webglCanvasText/web/main.dart
+++ example_todo/webglCanvasText/web/main.dart
@@ -6,7 +6,20 @@ import 'package:umiuni2d/tinyutil_webgl.dart' as tiny;
 
 main() async {
   DivElement output = querySelector('#output');
-  CanvasElement  canvas = await tiny.CanvasElementText.makeImage("xxxxxx Game xxxxxx aaaaaa Game bbbbbb",align:tiny.CanvasElementTextAlign.left_top);
+  CanvasElement  canvas = await tiny.CanvasElementText.makeImage(
+/*    "xxxxxx Game xxxxxx aaaaaa Game bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam2 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam3 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam4 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam5 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam6 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam7 bbbbbb"+*/
+    "xxxxxx Game xxxxxx aaaaaa Gam8 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam9 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam10 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam11 bbbbbb"+
+    "xxxxxx Game xxxxxx aaaaaa Gam12 bbbbbb"
+    ,align:tiny.CanvasElementTextAlign.left_top);
 
 //  CanvasElement  canvas = await tiny.CanvasElementText.makeImage("xxxxxx Game xxxxxx" +" aaaaaa Game bbbbbb");
   canvas.style.backgroundColor = "#333333";
