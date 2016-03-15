387aec81be8e28f10e041db90a953192fba61005
kyorohiro <kyorohiro@gmail.com>
2016-03-15 22:21:43 +0900

    mod pushclip

--- example/pushClip/web/main.dart
+++ example/pushClip/web/main.dart
@@ -1,5 +1,5 @@
 import 'package:umiuni2d/tinygame.dart';
-import 'package:umiuni2d/tinygame_webgl.dart';
+import 'package:umiuni2d.webgl/tinygame_webgl.dart';
 import 'package:push_clip/push_clip.dart';
 
 void main() {
