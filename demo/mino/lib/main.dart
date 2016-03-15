15bb6c96c752455dc359f3480190070bc2bde7c7
kyorohiro <kyorohiro@gmail.com>
2016-03-15 20:58:41 +0900

    mod mino demo for mod umiuni2d library interface

--- demo/mino/lib/main.dart
+++ demo/mino/lib/main.dart
@@ -6,17 +6,19 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/rendering.dart';
 import 'package:umiuni2d/tinygame.dart';
-import 'package:umiuni2d/tinygame_flutter.dart';
+import 'package:umiuni2d.flutter/tinygame_flutter.dart';
 import 'package:mino/game.dart';
 
 void main() {
   runApp(new GameWidget());
 }
 
-class GameWidget extends OneChildRenderObjectWidget {
+class GameWidget extends SingleChildRenderObjectWidget {
   GameWidget() {}
-  RenderObject createRenderObject() {
-    TinyGameBuilderForFlutter builder = new TinyGameBuilderForFlutter(assetsRoot:"web/");
+
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    TinyGameBuilderForFlutter builder = new TinyGameBuilderForFlutter(assetsRoot: "web/");
     builder.tickInPerFrame = false;
     builder.useTestCanvas = true;
     TinyStage stage = builder.createStage(new MinoRoot(builder));
--- demo/mino/web/main.dart
+++ demo/mino/web/main.dart
@@ -1,5 +1,6 @@
 import 'package:umiuni2d/tinygame.dart';
-import 'package:umiuni2d/tinygame_webgl.dart';
+import 'package:umiuni2d.webgl/tinygame_webgl.dart';
+
 import 'package:mino/game.dart';
 
 main() async {
