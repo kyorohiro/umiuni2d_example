aac62f4f85675988189b0436d345f8f63dca1b79
kyorohiro <kyorohiro@gmail.com>
2016-03-15 23:42:46 +0900

    mod show test

--- example/drawSnow/web/main.dart
+++ example/drawSnow/web/main.dart
@@ -1,5 +1,5 @@
 import 'package:umiuni2d/tinygame.dart';
-import 'package:umiuni2d/tinygame_webgl.dart';
+import 'package:umiuni2d.webgl/tinygame_webgl.dart';
 //import 'package:vector_math/vector_math_64.dart';
 import 'package:snowtest/snow_test.dart';
 
