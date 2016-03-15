8cb6687b09fc69a5f6fc79eae5fa125d56d3531e
kyorohiro <kyorohiro@gmail.com>
2016-03-16 01:09:03 +0900

    mod draw primitive test

--- example/drawPrimitive/web/main.dart
+++ example/drawPrimitive/web/main.dart
@@ -1,5 +1,5 @@
 import 'package:umiuni2d/tinygame.dart';
-import 'package:umiuni2d/tinygame_webgl.dart';
+import 'package:umiuni2d.webgl/tinygame_webgl.dart';
 import 'package:draw_primitive/primitive_test.dart';
 
 void main() {
