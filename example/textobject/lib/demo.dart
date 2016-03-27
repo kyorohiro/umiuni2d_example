192b24da2ff46769adc26741c6b807af413bae73
kyorohiro <kyorohiro@gmail.com>
2016-03-28 00:36:22 +0900

    ..

--- example/textobject/lib/demo.dart
+++ example/textobject/lib/demo.dart
@@ -2,12 +2,10 @@ import 'package:umiuni2d/tinygame.dart';
 
 class GameRoot extends TinyDisplayObject {
   TinyGameBuilder builder;
-  TinySprite chara;
-  TinyStage stage = null;
   TinyTextObjcet text = null;
 
   GameRoot(this.builder) {
-    text = builder.createTextObject("abc", 100.0, 100.0, fontFamily: "Aclonica");
+    text = builder.createTextObject("abc Hello World!!", 100.0, 100.0, fontFamily: "Aclonica");
     text.backgroundColor = new TinyColor.argb(0xff, 0xff, 0xff, 0xff);
     addChild(text);
   }
--- example/textobject/web/main.dart
+++ example/textobject/web/main.dart
@@ -4,11 +4,34 @@
 import 'package:umiuni2d/tinygame.dart';
 import 'package:umiuni2d.webgl/tinygame_webgl.dart';
 import 'package:textobject_test/demo.dart';
+import 'dart:async';
 
 main() async {
+//  await new Future.delayed(new Duration(milliseconds: 2800));
   TinyGameBuilder builder = new TinyGameBuilderForWebgl();
-  GameRoot root = new GameRoot(builder);
+  TinyGameRoot root = new TinyGameRoot(400.0, 300.0);
+  root.addChild(new GameRoot(builder));
   TinyStage stage = builder.createStage(root);
-  root.stage = stage;
+
+  //TinyStage stage = builder.createStage(new GameRoot(builder));
   stage.markPaintshot();
 }
+/*
+class GameRoot extends TinyDisplayObject {
+  TinyGameBuilder builder;
+  TinyTextObjcet text = null;
+
+  GameRoot(this.builder) {
+    text = builder.createTextObject("ABC Hello World!!", 100.0, 100.0, fontFamily: "Aclonica");
+    text.backgroundColor = new TinyColor.argb(0xff, 0xff, 0xff, 0xff);
+    addChild(text);
+  }
+
+  void onAttach(TinyStage stage, TinyDisplayObject parent) {
+    text.updateText().then((_) async {
+//      await new Future.delayed(new Duration(seconds: 2));
+      stage.markPaintshot();
+    });
+  }
+}
+*/
